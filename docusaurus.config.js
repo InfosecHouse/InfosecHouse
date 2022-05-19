3d719f53e4ddbad04a19c5a880420ea4f970a9aa
Infosec House <admin@infosec.house>
2022-05-18 23:03:46 -0600

    adding trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Infosec House',
   tagline: 'Tools & Resources for Cyber Security Operations.',
   url: 'https://infosec.house',
-  baseUrl: '/website',
+  baseUrl: '/website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.png',
@@ -143,6 +143,6 @@ const config = {
 module.exports = 
 {
   url: 'https://infosechouse.netlify.app',
-  baseUrl: '/website', 
+  baseUrl: '/website/', 
   title: 'Infosec House'
 };
